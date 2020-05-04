2fcef0fee374c68ec901a0de81c548d7eb1233bb
Maicon de Menezes <mdmoliveira@inf.ufpel.edu.br>
2020-05-04 20:09:36 -0300

    versão funcional

---
+++ build/precache-manifest.13d7d83573a76e00d7e4ff78160149c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad220e23c67667c7df253e43294c9f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0c0879985233cab4411",
+    "url": "/static/css/main.cafefd92.chunk.css"
+  },
+  {
+    "revision": "4d92aaf7c71d564c7983",
+    "url": "/static/js/2.58a328c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.58a328c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c0879985233cab4411",
+    "url": "/static/js/main.2d16c894.chunk.js"
+  },
+  {
+    "revision": "d2a32f618b7f4ffe5931",
+    "url": "/static/js/runtime-main.c0fb9964.js"
+  }
+]);
